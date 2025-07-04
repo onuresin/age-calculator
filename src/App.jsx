653ba32f0052de2eabf6f2f56a63611704b67820
Onur Esin <onuresn034@gmail.com>
2025-07-04 13:37:03 +0300

    alternative branchi revize edildi. Proje tekrar çalışır halde. CSS düzeltildi"

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
-import { useState,useEffect} from "react";
+import { useState } from "react";
 import AgeCalculator from "./Components/AgeCalculator";
 
-
 function App() {
-  const [day, setDay] = useState(1);
-  const [month, setMonth] = useState(1);
-  const [year, setYear] = useState(1900);
-  const [today, setToday] = useState(new Date());
-  const [age, setAge] = useState(0);
-  useEffect(() => {
-    fetch("http://worldtimeapi.org/api/timezone/Europe/Istanbul")
-      .then((r) => r.json())
-      .then((data) => setToday(new Date(data.unixtime * 1000)));
-  }, []);
+  const [age, setAge] = useState({ years: "--", months: "--", days: "--" });
 
   return (
     <AgeCalculator 
-      day={day}
-      month={month}
-      year={year}
       setAge={setAge}
-      today={today}
       result={age}
     />
-    
   );
 }
 
